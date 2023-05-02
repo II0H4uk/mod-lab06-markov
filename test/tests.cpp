e13a358fe5dfad6499c5841acf76d72f8c6841a1
II0H4uk <126766004+II0H4uk@users.noreply.github.com>
2023-05-02 18:54:10 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,48 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
+#include "../include/MCHeader.h"
+#include "../src/MarkovChain.cpp"
+
+TEST(prefixCheck, test1) {
+    MarkovChain chain = MarkovChain("input.txt");
+    prefix prefixTest = chain.Get_statetab().begin()->first;
+    EXPECT_EQ(prefixTest.size(), NPREF);
+}
+
+TEST(prefixCheck, test2) {
+    MarkovChain chain = MarkovChain("input.txt");
+    prefix prefixTest = prefix();
+    prefixTest.push_back("С");
+    prefixTest.push_back("непростою");
+    EXPECT_TRUE(!chain.Get_statetab().find(prefixTest)->second.empty());
+}
+
+TEST(suffixCheck, test3) {
+    MarkovChain chain = MarkovChain("input.txt");
+    prefix prefixTest = prefix();
+    prefixTest.push_back("Старик");
+    prefixTest.push_back("ловил");
+    suffix suf = chain.Get_statetab().find(prefixTest)->second;
+    suffix expectedSuf = suffix();
+    expectedSuf.push_back("неводом");
+    EXPECT_EQ(suf, expectedSuf);
+}
+
+TEST(suffixCheck, test4) {
+    MarkovChain chain = MarkovChain("input.txt");
+    prefix prefixTest = prefix();
+    prefixTest.push_back("Так");
+    prefixTest.push_back("и");
+    suffix suf = chain.Get_statetab().find(prefixTest)->second;
+    suffix expectedSuf = suffix();
+    expectedSuf.push_back("быть");
+    expectedSuf.push_back("вздулись");
+    expectedSuf.push_back("ходят");
+    EXPECT_EQ(suf, expectedSuf);
+}
+
+TEST(generatorCheck, test5) {
+    MarkovChain chain = MarkovChain("input.txt");
+    vector<string> genText = chain.Generator(100);
+    EXPECT_EQ(genText.size(), 100);
+}
