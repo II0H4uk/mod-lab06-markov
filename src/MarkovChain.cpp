feb665d340bacc6b7764ec8ce6db8e5d088dd518
II0H4uk <126766004+II0H4uk@users.noreply.github.com>
2023-05-02 18:51:45 +0300

    Add files via upload

---
+++ src/MarkovChain.cpp
@@ -0,0 +1,82 @@
+#include "MCHeader.h"
+
+MarkovChain::MarkovChain(string path)
+{
+    vector<string> words = ReadFile(path);
+    statetab = map<prefix, vector<string>>();
+
+    prefix pref = prefix();
+
+    for (int i = 0; i < NPREF; i++)
+        pref.push_back(words[i]);
+    for (int i = NPREF; i < words.size(); i++)
+    {
+        if (statetab.find(pref) == statetab.end())
+            statetab[pref] = suffix();
+        statetab[pref].push_back(words[i]);
+        pref.pop_front();
+        pref.push_back(words[i]);
+    }
+}
+
+vector<string> MarkovChain::Generator(int maxLength)
+{
+    vector<string> buf = vector<string>();
+    prefix pref = statetab.begin()->first;
+    int num;
+    srand(time(NULL));
+
+    for (int i = 0; i < maxLength && statetab.find(pref) != statetab.end(); i++)
+    {
+        num = rand() % (statetab[pref].size());
+        suffix bufsuf = statetab[pref];
+        buf.push_back(bufsuf[num]);
+        pref.pop_front();
+        pref.push_back(bufsuf[num]);
+    }
+
+    return buf;
+}
+
+map<prefix, suffix> MarkovChain::Get_statetab()
+{
+    return statetab;
+}
+
+vector<string> MarkovChain::ReadFile(string path)
+{
+    ifstream read(path);
+    string word;
+    string buf;
+    vector<string> words;
+
+    while (getline(read, word))
+    {
+        buf = "";
+        for (int i = 0; ; i++)
+        {
+            if (word[i] == ' ' || word[i] == '.' || word[i] == ';' || word[i] == '!' || word[i] == ',' || word[i] == ':' || word[i] == '"')
+            {
+                if (buf != "")
+                {
+                    words.push_back(buf);
+                    buf = "";
+                }
+
+                if (word[i] != ' ')
+                {
+                    buf += word[i];
+                    words.push_back(buf);
+                    buf = "";
+                }
+            }
+            else
+                buf += word[i];
+            if (word[i] == '\0')
+                break;
+        }
+        words.push_back("\n");
+    }
+    read.close();
+    return words;
+}
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+﻿#include "MCHeader.h"
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    MarkovChain markChain = MarkovChain("input.txt");
+    vector<string> genText = markChain.Generator(MAXGEN);
+    for (auto word : genText)
+    {
+        if (word != "." && word != ";" && word != "!" && word != "," && word != ":")
+            cout << " ";
+        cout << word;
+    }
+    return 0;
+}
