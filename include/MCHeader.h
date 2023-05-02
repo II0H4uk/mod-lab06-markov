0800fd69bbe68f24f12af2449717a340f207c790
II0H4uk <126766004+II0H4uk@users.noreply.github.com>
2023-05-02 18:47:13 +0300

    Add header

---
+++ include/MCHeader.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <deque>
+#include <fstream>
+
+using std::deque;
+using std::map;
+using std::vector;
+using std::string;
+using std::ifstream;
+using std::cout;
+
+const int NPREF = 2;
+const int MAXGEN = 1000;
+
+typedef deque<string> prefix;
+typedef vector<string> suffix;
+
+class MarkovChain
+{
+private:
+    map<prefix, suffix> statetab;
+
+    vector<string> ReadFile(string path);
+public:
+    MarkovChain(string path);
+
+    vector<string> Generator(int maxLength);
+    map<prefix, suffix> Get_statetab();
+};
+
+
