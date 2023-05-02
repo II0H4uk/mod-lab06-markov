01d8ff47a636beead6dfaede50169426ebfdd226
II0H4uk <126766004+II0H4uk@users.noreply.github.com>
2023-05-02 19:10:59 +0300

    Update and rename MarkovChain.cpp to textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -1,7 +1,7 @@
-#include "MCHeader.h"
+// Copyright 2021 GHA Test Team
+#include "textgen.h"
 
-MarkovChain::MarkovChain(string path)
-{
+MarkovChain::MarkovChain(string path) {
     vector<string> words = ReadFile(path);
     statetab = map<prefix, vector<string>>();
 
@@ -9,8 +9,7 @@ MarkovChain::MarkovChain(string path)
 
     for (int i = 0; i < NPREF; i++)
         pref.push_back(words[i]);
-    for (int i = NPREF; i < words.size(); i++)
-    {
+    for (int i = NPREF; i < words.size(); i++) {
         if (statetab.find(pref) == statetab.end())
             statetab[pref] = suffix();
         statetab[pref].push_back(words[i]);
@@ -19,16 +18,14 @@ MarkovChain::MarkovChain(string path)
     }
 }
 
-vector<string> MarkovChain::Generator(int maxLength)
-{
+vector<string> MarkovChain::Generator(int maxLength) {
     vector<string> buf = vector<string>();
     prefix pref = statetab.begin()->first;
     int num;
     srand(time(NULL));
 
-    for (int i = 0; i < maxLength && statetab.find(pref) != statetab.end(); i++)
-    {
-        num = rand() % (statetab[pref].size());
+    for (int i = 0; i < maxLength && statetab.find(pref) != statetab.end(); i++) {
+        num = rand_r() % (statetab[pref].size());
         suffix bufsuf = statetab[pref];
         buf.push_back(bufsuf[num]);
         pref.pop_front();
@@ -38,39 +35,32 @@ vector<string> MarkovChain::Generator(int maxLength)
     return buf;
 }
 
-map<prefix, suffix> MarkovChain::Get_statetab()
-{
+map<prefix, suffix> MarkovChain::Get_statetab() {
     return statetab;
 }
 
-vector<string> MarkovChain::ReadFile(string path)
-{
+vector<string> MarkovChain::ReadFile(string path) {
     ifstream read(path);
     string word;
     string buf;
     vector<string> words;
 
-    while (getline(read, word))
-    {
+    while (getline(read, word)) {
         buf = "";
-        for (int i = 0; ; i++)
-        {
-            if (word[i] == ' ' || word[i] == '.' || word[i] == ';' || word[i] == '!' || word[i] == ',' || word[i] == ':' || word[i] == '"')
-            {
-                if (buf != "")
-                {
+        for (int i = 0; ; i++) {
+            if (word[i]==' '||word[i]=='.'||word[i]==';'||word[i]
+                =='!'||word[i]==','||word[i]==':'||word[i]=='"') {
+                if (buf != "") {
                     words.push_back(buf);
                     buf = "";
                 }
 
-                if (word[i] != ' ')
-                {
+                if (word[i] != ' ') {
                     buf += word[i];
                     words.push_back(buf);
                     buf = "";
                 }
-            }
-            else
+            } else
                 buf += word[i];
             if (word[i] == '\0')
                 break;
