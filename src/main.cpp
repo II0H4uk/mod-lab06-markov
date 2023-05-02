471f8c3d6a5743f0da92218297c33ed7399294e0
II0H4uk <126766004+II0H4uk@users.noreply.github.com>
2023-05-02 19:13:43 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
-﻿#include "MCHeader.h"
+﻿// Copyright 2021 GHA Test Team
+#include "textgen.h"
 
 int main() {
     setlocale(LC_ALL, "rus");
     MarkovChain markChain = MarkovChain("input.txt");
     vector<string> genText = markChain.Generator(MAXGEN);
-    for (auto word : genText)
-    {
-        if (word != "." && word != ";" && word != "!" && word != "," && word != ":")
+    for (auto word : genText) {
+        if (word!="."&&word!=";"&&word!="!"&&word!=","&&word!=":")
             cout << " ";
         cout << word;
     }
